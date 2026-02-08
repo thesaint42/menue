aa64e6b431ffb4e1ba836adc6cb3dd2b7dc95d71
thesaint42 <olaf@schneider-ret.de>
2026-02-08 19:35:24 +0100

    chore(login): include central footer (Impressum/Datenschutz) on admin login

--- admin/login.php
+++ admin/login.php
@@ -90,5 +90,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+                <?php include __DIR__ . '/../nav/footer.php'; ?>
 </body>
 </html>
